7681bf8248cf39f2f6ed8fe664e3bf20df2337d8
Omid Alizadeh <om_alizadeh@yahoo.com>
2021-02-05 22:10:20 +0330

    feat: publish resources

--- src/Providers/MultiPaymentServiceProvider.php
+++ src/Providers/MultiPaymentServiceProvider.php
@@ -10,22 +10,25 @@ class MultiPaymentServiceProvider extends ServiceProvider
     {
         parent::register();
         $this->mergeConfigFrom(
-            __DIR__ . '/../config/online-payment.php.php',
+            __DIR__ . '/../../config/online-payment.php',
             'online-payment.php'
         );
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'MultiPayment');
     }
 
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->publishes(
-                [
-                    __DIR__ . '/../config/online-payment.php' => config_path(
-                        'online-payment.php'
-                    )
-                ],
-                'config'
-            );
+            $this->publishes([
+                __DIR__ . '/../../config/online-payment.php' => config_path(
+                    'online-payment.php'
+                )
+            ], 'config');
+            $this->publishes([
+                __DIR__ . '/../../resources/views' => resource_path(
+                    'views/vendor/multipayment'
+                )
+            ], 'views');
         }
     }
 }
