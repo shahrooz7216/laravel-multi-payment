3a659b73b4673ee53aa6ea526a28e94b13cac0a8
Omid Alizadeh <om_alizadeh@yahoo.com>
2021-04-05 22:56:39 +0430

    fix: extend

--- src/Exceptions/PaymentAlreadyVerifiedException.php
+++ src/Exceptions/PaymentAlreadyVerifiedException.php
@@ -2,9 +2,7 @@
 
 namespace Omalizadeh\MultiPayment\Exceptions;
 
-use Exception;
-
-class PaymentAlreadyVerifiedException extends Exception
+class PaymentAlreadyVerifiedException extends PaymentFailedException
 {
     //
 }
