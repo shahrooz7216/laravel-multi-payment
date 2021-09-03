d839a0f6815192b9bf498dd34a49c7509facedc6
Omid <om_alizadeh@yahoo.com>
2021-09-04 00:42:44 +0430

    feat: change zarinpal payment method to REST

--- src/Drivers/Zarinpal/Zarinpal.php
+++ src/Drivers/Zarinpal/Zarinpal.php
@@ -2,34 +2,33 @@
 
 namespace Omalizadeh\MultiPayment\Drivers\Zarinpal;
 
-use SoapClient;
+use Illuminate\Support\Facades\Http;
 use Omalizadeh\MultiPayment\RedirectionForm;
 use Omalizadeh\MultiPayment\Drivers\Contracts\Driver;
+use Omalizadeh\MultiPayment\Exceptions\HttpRequestFailedException;
 use Omalizadeh\MultiPayment\Exceptions\PaymentFailedException;
 use Omalizadeh\MultiPayment\Exceptions\PurchaseFailedException;
 use Omalizadeh\MultiPayment\Exceptions\InvalidConfigurationException;
 use Omalizadeh\MultiPayment\Exceptions\PaymentAlreadyVerifiedException;
+use Omalizadeh\MultiPayment\Receipt;
 
 class Zarinpal extends Driver
 {
     public function purchase(): string
     {
-        $data = $this->getPurchaseData();
-        $soapOptions = $this->getSoapOptions();
-        $client = new SoapClient($this->getPurchaseUrl(), $soapOptions);
-        $result = $client->PaymentRequest($data);
-        if ($result->Status != $this->getSuccessResponseStatusCode() or empty($result->Authority)) {
-            $message = $this->getStatusMessage($result->Status);
-            throw new PurchaseFailedException($message, $result->Status);
+        $response = $this->callApi($this->getPurchaseUrl(), $this->getPurchaseData());
+        if ($response['data']['code'] !== $this->getSuccessResponseStatusCode() or empty($response['data']['authority'])) {
+            $message = $this->getStatusMessage($response['data']['code']);
+            throw new PurchaseFailedException($message, $response['data']['code']);
         }
-        $this->invoice->setTransactionId($result->Authority);
+        $this->getInvoice()->setTransactionId($response['data']['authority']);
 
-        return $result->Authority;
+        return $response['data']['authority'];
     }
 
     public function pay(): RedirectionForm
     {
-        $transactionId = $this->invoice->getTransactionId();
+        $transactionId = $this->getInvoice()->getTransactionId();
         $paymentUrl = $this->getPaymentUrl();
         if (strtolower($this->getMode()) == 'zaringate') {
             $payUrl = str_replace(':authority', $transactionId, $paymentUrl);
@@ -37,29 +36,33 @@ class Zarinpal extends Driver
             $payUrl = $paymentUrl . $transactionId;
         }
 
-        return $this->redirectWithForm($payUrl, [], 'GET');
+        return $this->redirect($payUrl, [], 'GET');
     }
 
-    public function verify(): string
+    public function verify(): Receipt
     {
         $status = request('Status');
-        if ($status != 'OK') {
+
+        if ($status !== 'OK') {
             throw new PaymentFailedException('عملیات پرداخت ناموفق بود یا توسط کاربر لغو شد.');
         }
-        $data = $this->getVerificationData();
-        $soapOptions = $this->getSoapOptions();
-        $client = new SoapClient($this->getVerificationUrl(), $soapOptions);
-        $result = $client->PaymentVerification($data);
-        if ($result->Status != $this->getSuccessResponseStatusCode()) {
-            $message = $this->getStatusMessage($result->Status);
-            if ($result->Status == $this->getPaymentAlreadyVerifiedStatusCode()) {
-                throw new PaymentAlreadyVerifiedException($message, $result->Status);
-            }
-            throw new PaymentFailedException($message, $result->Status);
-        }
-        $this->invoice->setReferenceId(request('Authority'));
 
-        return $result->RefID;
+        $response = $this->callApi($this->getVerificationUrl(), $this->getVerificationData());
+        $responseCode = $response['data']['code'];
+
+        if ($responseCode !== $this->getSuccessResponseStatusCode()) {
+            $message = $this->getStatusMessage($responseCode);
+            if ($responseCode == $this->getPaymentAlreadyVerifiedStatusCode()) {
+                throw new PaymentAlreadyVerifiedException($message, $responseCode);
+            }
+            throw new PaymentFailedException($message, $responseCode);
+        }
+
+        $refId = $response['data']['ref_id'];
+
+        $this->getInvoice()->setReferenceId($refId);
+
+        return new Receipt($this->getInvoice(), $refId, $refId);
     }
 
     protected function getPurchaseData(): array
@@ -67,66 +70,69 @@ class Zarinpal extends Driver
         if (empty($this->settings['merchant_id'])) {
             throw new InvalidConfigurationException('Merchant id has not been set.');
         }
-        if (!empty($this->invoice->getDescription())) {
-            $description = $this->invoice->getDescription();
+        if (!empty($this->getInvoice()->getDescription())) {
+            $description = $this->getInvoice()->getDescription();
         } else {
             $description = $this->settings['description'];
         }
-        $mobile = $this->invoice->getPhoneNumber();
-        $email = $this->invoice->getEmail();
+        $mobile = $this->getInvoice()->getPhoneNumber();
+        $email = $this->getInvoice()->getEmail();
 
         return [
-            'MerchantID' => $this->settings['merchant_id'],
-            'Amount' => $this->invoice->getAmountInTomans(),
-            'CallbackURL' => $this->settings['callback_url'],
-            'Description' => $description,
-            'Mobile' => $mobile,
-            'Email' => $email,
+            'merchant_id' => $this->settings['merchant_id'],
+            'amount' => $this->getInvoice()->getAmount(),
+            'callback_url' => $this->settings['callback_url'],
+            'description' => $description,
+            'meta_data' => [
+                'mobile' => $mobile,
+                'email' => $email
+            ]
         ];
     }
 
     protected function getVerificationData(): array
     {
-        $authority = request('Authority') ?? $this->invoice->getTransactionId();
+        $authority = request('Authority', $this->getInvoice()->getTransactionId());
+
         return [
-            'MerchantID' => $this->settings['merchant_id'],
-            'Authority' => $authority,
-            'Amount' => $this->invoice->getAmountInTomans(),
+            'merchant_id' => $this->settings['merchant_id'],
+            'authority' => $authority,
+            'amount' => $this->getInvoice()->getAmount(),
         ];
     }
 
     protected function getStatusMessage($statusCode): string
     {
         $messages = [
-            "-1" => "اطلاعات ارسال شده ناقص است.",
-            "-2" => "IP و يا مرچنت كد پذيرنده صحيح نيست",
-            "-3" => "با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد",
-            "-4" => "سطح تاييد پذيرنده پايين تر از سطح نقره اي است.",
-            "-11" => "درخواست مورد نظر يافت نشد.",
-            "-12" => "امكان ويرايش درخواست ميسر نمي باشد.",
-            "-21" => "هيچ نوع عمليات مالي براي اين تراكنش يافت نشد",
-            "-22" => "تراكنش نا موفق ميباشد",
-            "-33" => "رقم تراكنش با رقم پرداخت شده مطابقت ندارد",
-            "-34" => "سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است",
-            "-40" => "اجازه دسترسي به متد مربوطه وجود ندارد.",
-            "-41" => "اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.",
-            "-42" => "مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.",
-            "-54" => "درخواست مورد نظر آرشيو شده است",
-            "101" => "عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.",
+            -1 => "اطلاعات ارسال شده ناقص است.",
+            -2 => "IP و يا مرچنت كد پذيرنده صحيح نيست",
+            -3 => "با توجه به محدوديت هاي شاپرك امكان پرداخت با رقم درخواست شده ميسر نمي باشد",
+            -4 => "سطح تاييد پذيرنده پايين تر از سطح نقره اي است.",
+            -11 => "درخواست مورد نظر يافت نشد.",
+            -12 => "امكان ويرايش درخواست ميسر نمي باشد.",
+            -21 => "هيچ نوع عمليات مالي براي اين تراكنش يافت نشد",
+            -22 => "تراكنش نا موفق ميباشد",
+            -33 => "رقم تراكنش با رقم پرداخت شده مطابقت ندارد",
+            -34 => "سقف تقسيم تراكنش از لحاظ تعداد يا رقم عبور نموده است",
+            -40 => "اجازه دسترسي به متد مربوطه وجود ندارد.",
+            -41 => "اطلاعات ارسال شده مربوط به AdditionalData غيرمعتبر ميباشد.",
+            -42 => "مدت زمان معتبر طول عمر شناسه پرداخت بايد بين 30 دقيه تا 45 روز مي باشد.",
+            -54 => "درخواست مورد نظر آرشيو شده است",
+            101 => "عمليات پرداخت موفق بوده و قبلا PaymentVerification تراكنش انجام شده است.",
         ];
         $unknownError = 'خطای ناشناخته رخ داده است.';
 
         return array_key_exists($statusCode, $messages) ? $messages[$statusCode] : $unknownError;
     }
 
-    protected function getSuccessResponseStatusCode(): string
+    protected function getSuccessResponseStatusCode(): int
     {
-        return "100";
+        return 100;
     }
 
-    private function getPaymentAlreadyVerifiedStatusCode(): string
+    private function getPaymentAlreadyVerifiedStatusCode(): int
     {
-        return "101";
+        return 101;
     }
 
     protected function getPurchaseUrl(): string
@@ -134,10 +140,10 @@ class Zarinpal extends Driver
         $mode = $this->getMode();
         switch ($mode) {
             case 'sandbox':
-                $url = 'https://sandbox.zarinpal.com/pg/services/WebGate/wsdl';
+                $url = 'https://sandbox.zarinpal.com/pg/v4/payment/request.json';
                 break;
             default:
-                $url = 'https://ir.zarinpal.com/pg/services/WebGate/wsdl';
+                $url = 'https://api.zarinpal.com/pg/v4/payment/request.json';
                 break;
         }
 
@@ -167,10 +173,10 @@ class Zarinpal extends Driver
         $mode = $this->getMode();
         switch ($mode) {
             case 'sandbox':
-                $url = 'https://sandbox.zarinpal.com/pg/services/WebGate/wsdl';
+                $url = 'https://sandbox.zarinpal.com/pg/v4/payment/verify.json';
                 break;
             default:
-                $url = 'https://ir.zarinpal.com/pg/services/WebGate/wsdl';
+                $url = 'https://api.zarinpal.com/pg/v4/payment/verify.json';
                 break;
         }
 
@@ -182,10 +188,24 @@ class Zarinpal extends Driver
         return strtolower(trim($this->settings['mode']));
     }
 
-    private function getSoapOptions(): array
+    private function getRequestHeaders(): array
     {
-        return config('gateway_zarinpal.soap_options', [
-            'encoding' => 'UTF-8'
+        return config('gateway_zarinpal.request_headers', [
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
         ]);
+    }
+
+    private function callApi(string $url, array $data)
+    {
+        $headers = $this->getRequestHeaders();
+
+        $response = Http::withHeaders($headers)->post($url, $data);
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        throw new HttpRequestFailedException($response->body(), $response->status());
     }
 }
