d60e0df079f61069018d8105b34c6dd378bc8505
Omid <om_alizadeh@yahoo.com>
2022-01-27 19:25:53 +0330

    chore: add authorization_token to config

--- config/gateway_zarinpal.php
+++ config/gateway_zarinpal.php
@@ -11,12 +11,14 @@ return [
      *  gateway configurations
      */
     'main' => [
+        'authorization_token' => '', // Used only to refund payments and can be created from zarinpal panel
         'merchant_id' => '',
         'callback_url' => 'https://yoursite.com/path/to',
         'mode' => 'sandbox', // Supported values: normal, zaringate, sandbox
         'description' => 'payment using zarinpal',
     ],
     'other' => [
+        'authorization_token' => '',
         'merchant_id' => '',
         'callback_url' => 'https://yoursite.com/path/to',
         'mode' => 'normal',
